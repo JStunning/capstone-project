a1d3b3b8e0a4baa0c5c9e4fe72c1484bbe030c4e
Jack Dunning <JackStunning9001@gmail.com>
2020-05-08 10:14:17 -0700

    Add radio buttons

--- src/components/SearchItemList.js
+++ src/components/SearchItemList.js
@@ -5,6 +5,22 @@ function SearchItemList() {
     <React.Fragment>
       <div className='SearchItemList'>
         <h2>Search Item List</h2>
+
+        <label for="stamina">Stamina</label>
+        <input type="radio" id="stamina" name="stamina" value="stamina" />
+
+        <label for="strength">Strength</label>
+        <input type="radio" id="strength" name="strength" value="strength" />
+
+        <label for="dexterity">Dexterity</label>
+        <input type="radio" id="dexterity" name="dexterity" value="dexterity" />
+
+        <label for="intelligence">Intelligence</label>
+        <input type="radio" id="intelligence" name="intelligence" value="intelligence" />
+
+        <label for="wisdom">Wisdom</label>
+        <input type="radio" id="wisdom" name="wisdom" value="wisdom" />
+        
         <ul>
           <li>Item 1</li>
           <li>Item 2</li>
